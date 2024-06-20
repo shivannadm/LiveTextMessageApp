1c037f0647eda6bc99d5f7208f20b33531182688
Shivanna <shivannadm6@gmail.com>
2024-06-20 20:27:11 +0530

    Update MainActivity.java

--- app/src/main/java/com/android/myapplication/MainActivity.java
+++ app/src/main/java/com/android/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.android.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,10 +22,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.button);
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"LOGIN SUCCESFUL",Toast.LENGTH_SHORT).show();
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
+
         });
     }
 }
