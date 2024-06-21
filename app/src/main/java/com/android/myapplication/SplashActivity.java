95e2b6f039baa3935ad3f756264f50a7514f1681
Shivanna <shivannadm6@gmail.com>
2024-06-21 21:38:38 +0530

    Updated

--- app/src/main/java/com/android/myapplication/SplashActivity.java
+++ app/src/main/java/com/android/myapplication/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.android.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,7 +16,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Finish SplashActivity so the user cannot return to it
+            }
+        }, 4000); // 4000 milliseconds delay
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
