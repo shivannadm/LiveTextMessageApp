fc05586ac603ec5aea0f9f3035690c0941b75d35
Shivanna <shivannadm6@gmail.com>
2024-06-27 20:47:23 +0530

    Updated

--- app/src/main/java/com/android/myapplication/LoginNumberActivity.java
+++ app/src/main/java/com/android/myapplication/LoginNumberActivity.java
@@ -1,8 +1,6 @@
 package com.android.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,24 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SplashActivity extends AppCompatActivity {
+public class LoginNumberActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-
-        setContentView(R.layout.activity_splash);
-
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-//                startActivity(intent);
-//                finish(); // Finish SplashActivity so the user cannot return to it
-//            }
-//        }, 4000); // 4000 milliseconds delay
-
+        setContentView(R.layout.activity_login_number);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
