152c8ca6ab050395e0931c9e883e24002ad539d7
Shivanna <shivannadm6@gmail.com>
2024-06-28 20:11:13 +0530

    updated

--- app/src/main/java/com/android/myapplication/SplashActivity.java
+++ app/src/main/java/com/android/myapplication/SplashActivity.java
@@ -19,14 +19,14 @@ public class SplashActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_splash);
 
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-//                startActivity(intent);
-//                finish(); // Finish SplashActivity so the user cannot return to it
-//            }
-//        }, 4000); // 4000 milliseconds delay
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginNumberActivity.class);
+                startActivity(intent);
+                finish(); // Finish SplashActivity so the user cannot return to it
+            }
+        }, 3000); // 4000 milliseconds delay
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
