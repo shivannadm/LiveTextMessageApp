c1f69376f0921f28ae9a586017c445076a917fd6
Shivanna <shivannadm6@gmail.com>
2024-06-28 22:01:35 +0530

    updated

--- app/src/main/java/com/android/myapplication/LoginNumberActivity.java
+++ app/src/main/java/com/android/myapplication/LoginNumberActivity.java
@@ -1,6 +1,11 @@
 package com.android.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +13,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.hbb20.CountryCodePicker;
+
 public class LoginNumberActivity extends AppCompatActivity {
+
+    CountryCodePicker countryCodePicker;
+    EditText phoneInput;
+    Button sendOtpBtn;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_number);
+
+        countryCodePicker = findViewById(R.id.login_countrycode);
+        phoneInput = findViewById(R.id.login_mobile_number);
+        sendOtpBtn = findViewById(R.id.send_otp_btn);
+        progressBar = findViewById(R.id.login_progressbar);
+
+        progressBar.setVisibility(View.GONE);
+        countryCodePicker.registerCarrierNumberEditText(phoneInput);
+        sendOtpBtn.setOnClickListener((V)->{
+            if(!countryCodePicker.isValidFullNumber()){
+                phoneInput.setError("Phone number is not valid");
+                return;
+            }
+            Intent intent  = new Intent(LoginNumberActivity.this,LoginOtpActivity.class);
+            intent.putExtra("phone",countryCodePicker.getFullNumberWithPlus());
+            startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/android/myapplication/LoginOtpActivity.java
+++ app/src/main/java/com/android/myapplication/LoginOtpActivity.java
@@ -1,6 +1,7 @@
 package com.android.myapplication;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginOtpActivity extends AppCompatActivity {
 
+    String phoneNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_otp);
+
+        phoneNumber = getIntent().getExtras().getString("phone");
+        Toast.makeText(getApplicationContext(),phoneNumber,Toast.LENGTH_LONG).show();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
