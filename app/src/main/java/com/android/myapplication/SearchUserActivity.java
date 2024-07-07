cb56ea0a4c977c252c2d7d9d53034395dec9f891
Shivanna <shivannadm6@gmail.com>
2024-07-07 19:16:02 +0530

    Update SearchUserActivity.java

--- app/src/main/java/com/android/myapplication/SearchUserActivity.java
+++ app/src/main/java/com/android/myapplication/SearchUserActivity.java
@@ -1,13 +1,14 @@
 package com.android.myapplication;
 //
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 //import androidx.recyclerview.widget.LinearLayoutManager;
 //import androidx.recyclerview.widget.RecyclerView;
 //
-//import android.os.Bundle;
-//import android.util.Log;
-//import android.widget.EditText;
-//import android.widget.ImageButton;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageButton;
 //
 //import com.android.myapplication.adapter.SearchUserRecyclerAdapter;
 //import com.android.myapplication.model.UserModel;
@@ -17,40 +18,40 @@ import androidx.appcompat.app.AppCompatActivity;
 //
 public class SearchUserActivity extends AppCompatActivity {
 //
-//    EditText searchInput;
-//    ImageButton searchButton;
-//    ImageButton backButton;
-//    RecyclerView recyclerView;
+    EditText searchInput;
+    ImageButton searchButton;
+    ImageButton backButton;
+    RecyclerView recyclerView;
 //
 //    SearchUserRecyclerAdapter adapter;
 //
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_search_user);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_user);
+
+        searchInput = findViewById(R.id.seach_username_input);
+        searchButton = findViewById(R.id.search_user_btn);
+        backButton = findViewById(R.id.back_btn);
+        recyclerView = findViewById(R.id.search_user_recycler_view);
 //
-//        searchInput = findViewById(R.id.seach_username_input);
-//        searchButton = findViewById(R.id.search_user_btn);
-//        backButton = findViewById(R.id.back_btn);
-//        recyclerView = findViewById(R.id.search_user_recycler_view);
+        searchInput.requestFocus();
 //
-//        searchInput.requestFocus();
+        backButton.setOnClickListener(v -> {
+            onBackPressed();
+        });
+
+        searchButton.setOnClickListener(v -> {
+            String searchTerm = searchInput.getText().toString();
+            if (searchTerm.isEmpty() || searchTerm.length() < 3) {
+                searchInput.setError("Invalid Username");
+                return;
+            }
+            setupSearchRecyclerView(searchTerm);
+        });
+    }
 //
-//        backButton.setOnClickListener(v -> {
-//            onBackPressed();
-//        });
-//
-//        searchButton.setOnClickListener(v -> {
-//            String searchTerm = searchInput.getText().toString();
-//            if (searchTerm.isEmpty() || searchTerm.length() < 3) {
-//                searchInput.setError("Invalid Username");
-//                return;
-//            }
-//            setupSearchRecyclerView(searchTerm);
-//        });
-//    }
-//
-//    void setupSearchRecyclerView(String searchTerm) {
+    void setupSearchRecyclerView(String searchTerm) {
 //        Query query = FirebaseUtil.allUserCollectionReference()
 //                .whereGreaterThanOrEqualTo("username", searchTerm)
 //                .whereLessThanOrEqualTo("username", searchTerm + '\uf8ff');
@@ -69,7 +70,7 @@ public class SearchUserActivity extends AppCompatActivity {
 //        adapter.startListening();
 //
 //        Log.d("SearchUserActivity", "Query setup with search term: " + searchTerm);
-//    }
+    }
 //
 //    @Override
 //    protected void onStart() {
@@ -93,5 +94,5 @@ public class SearchUserActivity extends AppCompatActivity {
 //        if (adapter != null) {
 //            adapter.startListening();
 //        }
-//    }
+
 }
